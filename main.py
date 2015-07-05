8e9358010b05bfec775018a3d8a744555e5860f7
Robbie Harwood (frozencemetery) <rharwood@club.cc.cmu.edu>
2015-07-04 20:29:19 -0400

    Update model on queue

--- main.py
+++ main.py
@@ -3,37 +3,34 @@ import argparse
 import os
 import sys
 
-allowed = "qwertyuiopasdfghjklzxcvbnm"
+allowed = "qwertyuiopasdfghjklzxcvbnm.QWERTYUIOPASDFGHJKLZXCVBNM1234567890_-"
 
-def fix_fname(e):
+def fix_fname(e, full):
   newe = None
 
   for i in xrange(len(e)):
     if e[i] not in allowed:
-      if newe == None:
+      if not newe:
         # strings do not allow modification because reasons?
         newe = list(e)
-        if args.default:
-          newe[i] = args.default
-          pass
-        else:
-          print "TODO(frozen) banned character with no replacement!"
-          pass
+        pass
+      if args.default:
+        newe[i] = args.default
+        pass
+      else:
+        print "TODO(frozen) banned character with no replacement!"
         pass
       pass
     pass
-  print e
 
-  if newe:
-    print ''.join(newe)
-    pass
-
-  return ''.join(newe) if newe else e
+  return ''.join(newe) if newe else None
 
 def process_files(queue):
   while len(queue) > 0:
-    e = queue.pop()
-    if not os.path.lexists(e):
+    (lead, e) = queue.pop()
+    full = lead + e
+    print full
+    if not os.path.lexists(full):
       print "Path %s doesn't exist!" % e
       return 1
 
@@ -41,13 +38,16 @@ def process_files(queue):
     # about.  If present, they will have been passed by the user since we're
     # storing relative paths and os.listdir will not return them.
     if e not in [".", ".."]:
-      newe = fix_fname(e)
-      if newe != e:
+      newe = fix_fname(e, full)
+      if newe:
+        full = lead + newe
         print "TODO(frozen) Would rename `%s' to `%s'" % (e, newe)
+        e = newe
+        pass
       pass
 
-    if os.path.isdir(e):
-      queue += ["%s/%s" % (e, d) for d in os.listdir(e)]
+    if os.path.isdir(full):
+      queue += [(full + '/', d) for d in os.listdir(full)]
       pass
     pass
   return 0
@@ -62,7 +62,7 @@ def main(argv):
                  help="Default character for replacement (default: off)")
   args = p.parse_args(argv[1:])
   print args
-  sys.exit(process_files(args.targets))
+  sys.exit(process_files([("", s) for s in args.targets]))
 
 if __name__ == "__main__":
   main(sys.argv)
